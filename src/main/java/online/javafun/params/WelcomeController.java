71a41c4a063379120a237a875c4cb33db6e2425c
User <user@its.me>
2023-01-19 11:02:12 +0100

    print user metadata

--- src/main/java/online/javafun/params/WelcomeController.java
+++ src/main/java/online/javafun/params/WelcomeController.java
@@ -3,6 +3,7 @@ package online.javafun.params;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,11 +12,9 @@ public class WelcomeController {
 
     @GetMapping("/hello")
     @ResponseBody
-    String hello(@RequestParam(required = false) String name) {
-        if (name != null) {
-            return "Hello " + name;
-        } else {
-            return "Hello Stranger";
-        }
+    String hello(@RequestParam(required = false, defaultValue = "Stranger") String name,
+                 @RequestHeader("User-Agent") String userAgent) {
+        System.out.println("User Agent klienta: " + userAgent);
+        return "Hello " + name;
     }
 }
