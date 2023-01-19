54664bb4e8c1e271acdba63c711ac8bef3faf337
User <user@its.me>
2023-01-19 10:18:34 +0100

    Initial commit

---
+++ src/test/java/online/javafun/mvcparams/MvcParamsApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.mvcparams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcParamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
