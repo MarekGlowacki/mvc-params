0b82369ab8e1cacd74e02763a3a130e5699c3b9f
User <user@its.me>
2023-01-19 10:54:38 +0100

    parameter not required

--- src/main/java/online/javafun/params/WelcomeController.java
+++ src/main/java/online/javafun/params/WelcomeController.java
@@ -3,6 +3,7 @@ package online.javafun.params;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,8 +11,11 @@ public class WelcomeController {
 
     @GetMapping("/hello")
     @ResponseBody
-    String hello(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        return "Hello " + name;
+    String hello(@RequestParam(required = false) String name) {
+        if (name != null) {
+            return "Hello " + name;
+        } else {
+            return "Hello Stranger";
+        }
     }
 }
