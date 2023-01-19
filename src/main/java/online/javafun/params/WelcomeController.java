9f70e53b0d6f640e9aa4c38c9bbd51a62baced43
User <user@its.me>
2023-01-19 10:29:08 +0100

    Get method by Get mapping

--- src/main/java/online/javafun/params/WelcomeController.java
+++ src/main/java/online/javafun/params/WelcomeController.java
@@ -1,14 +1,13 @@
 package online.javafun.params;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
 
-    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
     @ResponseBody
     String hello() {
         return "Hello Stranger";
